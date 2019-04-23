0214825e69ddde92c0509922ea39d3934189d8ed
Carl Durand <carldurand.student@careerdevs.com>
2019-04-23 03:42:46 +0000

    Debugging: Use the JavaScript Console to Check the Value of a Variable

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-const result = {
-  success: ["max-length", "no-amd", "prefer-arrow-functions"],
-  failure: ["no-var", "var-on-top", "linebreak"],
-  skipped: ["id-blacklist", "no-dup-keys"]
-};
-function makeList(arr) {
-  "use strict";
+let a = 5;
+let b = 1;
+a++;
+// Add your code below this line
+console.log(a);
 
-  // change code below this line
-    const resultDisplayArray = arr.map((text) => {
-      return `<li class="text-warning">${text}</li>`
-    });
-  // change code above this line
-
-  return resultDisplayArray;
-}
-/**
- * makeList(result.failure) should return:
- * [ `<li class="text-warning">no-var</li>`,
- *   `<li class="text-warning">var-on-top</li>`, 
- *   `<li class="text-warning">linebreak</li>` ]
- ***/
-const resultDisplayArray = makeList(result.failure);
+let sumAB = a + b;
+console.log(sumAB);
